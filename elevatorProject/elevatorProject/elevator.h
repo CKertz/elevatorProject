36c19481266405d4d1d8bbd4ca8a9988f8066355
CKertz <kertzc@live.com>
2016-03-24 17:02:52 -0600

    More elevating chages

--- elevatorProject/elevatorProject/elevator.h
+++ elevatorProject/elevatorProject/elevator.h
@@ -28,56 +28,69 @@ int inputRequest(int floor) {
 } 
 //elevating will take a request from a user, stop at any floors requested between currentFloor and floorRequest,
 //pop these values from the vector, and continue in that direction.
-void elevating(int floorRequest) {//string givenDirection parameter? not sure yet
+//iterate through elevate, if floor between floorRequest and currentFloor exists in vector, stop at it
+//this means if iterator points at any variable greater than currentfloor, less than floorrequest
+void elevating(int floorRequest) 
+{ //string givenDirection parameter? not sure yet
 	vector<int>::iterator itr;
 	int i = 0;
-	for (itr = elevate.begin(); itr < elevate.end(); itr++, i++) //iterates vector, processes requests
-	{
+
 	if (elevate.size() == 0)
 		throw exception("No commands have been issued");
 	if (elevate.size() == 1)
+	{
+		currentFloor = floorRequest;
+		returnTime();
+		elevate.erase(elevate.begin());
+	}
+	for (itr = elevate.begin(); itr < elevate.end(); itr++, i++) //iterates vector, processes requests
+	{
+		if (*itr < currentFloor && direction == "down")
 		{
-			currentFloor = floorRequest;
-			returnTime();
-			elevate.erase(elevate.begin());
-		}
-	if (floorRequest < currentFloor)// needs to search for numbers ranging between floorRequest to currentFloor, visit, delete from elevate
-		{
-			direction = "down";
-			floorDifference = currentFloor - floorRequest;
-			waitingTime = floorDifference + 0.5*stopCount;
-			currentFloor -= floorRequest;
-
+			int *temp;
+			*temp = *itr;
+			delete temp;
 		}
 	}
-	
-	if (floorRequest > currentFloor) {
+			
+		/*if (floorRequest < currentFloor)// needs to search for numbers ranging between floorRequest to currentFloor, visit, delete from elevate
+			{
+				direction = "down";
+				//floorDifference = currentFloor - floorRequest;
+				//waitingTime = floorDifference + 0.5*stopCount;
+				currentFloor -= floorRequest;
+				
+
+			}*/
+	if (floorRequest > currentFloor)
+	{
 		direction = "up";
 		floorDifference = floorRequest - currentFloor;
 		currentFloor += floorRequest;
 	}
-
 }
-int returnTime() {
+
+
+int returnTime() 
+{
 	int timeCounter = 0;
 	int stopCount = 0;
 	int j = 0;
 
 	if (elevate.size() == 1)
 	{
-	for (int i = 0; i <= elevate[elevate.front()] ;  i++)
+		for (int i = 0; i <= elevate[elevate.front()]; i++)
 		{
 			//needs to keep track of stops
 			timeCounter++;
 			return timeCounter;
 		}
+	}
 	// need to get front, iterate and see if anything is between it and where it's going, increment time accordingly
 	vector<int>::iterator floorItr;
 	for (floorItr = elevate.begin(); floorItr < elevate.end(); floorItr++, j++)
 	{
 		
-	}
-	
 	}
 }
 };
