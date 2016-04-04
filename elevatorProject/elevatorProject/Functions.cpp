b3c1a1b2f83382072222c856ca55f064fe07001a
ChancellorBill-Harbinger <wgmpq4@mail.umkc.edu>
2016-04-04 14:14:46 -0500

    Added the generateUser function

--- elevatorProject/elevatorProject/Functions.cpp
+++ elevatorProject/elevatorProject/Functions.cpp
@@ -6,13 +6,37 @@ using namespace std;
 
 void simulation(Elevator elevator, vector<User>people)
 {
-	bool done = false;
+	int dice;
+	bool done = false; //determined if all users have arrived AND if the max number of generated users has been met
 	while (!done)
 	{
 		while (elevator.getFloor() != MAXFLOOR)
 		{
+
+			
 			elevator.goUp();
 		}
+		while (elevator.getFloor() != MINFLOOR)
+		{
+
+
+			elevator.goDown();
+		}
+	}
+}
+
+void generateUser(vector<User> people) // Used to create new people for the scenario
+{
+	int dice = rand() % 6 + 1; // Roll six sided dice to determine if a user is generated or not
+	if (dice % 2) // If even, add a user
+	{
+		User* temp = new User;
+		people.push_back(*temp);
+		delete temp;
+	}
+	else // If odd, return
+	{
+		return;
 	}
 }
 
